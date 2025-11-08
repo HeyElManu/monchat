63f6674729e08efb1d6dba8c43842d193fb4555c
Emmanuel Anthony <elmanu@MBP-de-Emmanuel.lan>
2025-11-08 18:47:07 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Un utilisateur est connecté');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => console.log('Serveur en ligne sur http://localhost:3000'));
